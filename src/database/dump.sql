272c23b30ca983eab3d8c0e5d20898608b2a7304
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-09 21:14:43 -0300

    chore!: criação e configuração do setup da api

---
+++ src/database/dump.sql
@@ -0,0 +1,22 @@
+create table usuarios(
+	id serial primary key,
+  nome text not null,
+  email text not null unique,
+  senha text not null
+);
+
+create table clientes(
+	id serial primary key,
+  nome text not null,
+  email text not null unique,
+  cpf text not null,
+  telefone int not null,
+  cep text,
+  logradouro text,
+  complemento text,
+  bairro text,
+  cidade text,
+  estado text,
+ 	usuario_id int,
+  foreign key (usuario_id) references usuarios(id)
+);
