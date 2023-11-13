b25329175639b34ce4b8f7b5aad45e55f78dac9b
T.Nuth <thiagomoreira@Mac-Pro-de-Thiago.local>
2023-11-13 18:07:56 -0300

    feat: criação da função Atualizar Usuario

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const router = require("./routes");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(bodyParser.json());
 
 app.listen(3000);
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+const updateUser = require("./validations/atualizarUsuario");
 const router = express();
+
 
 router.get("/", (req, res) => {
   res.send("Ola");
 });
+
+router.put("/usuarios/:id", updateUser);
 
 module.exports = router;
