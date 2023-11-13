38f6ac9413b8465cd8700f4bfe0ef0dd1c66f4cd
Isabella Brito <isadocarmo7@gmail.com>
2023-11-13 19:04:58 -0300

    Atualização validações

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -2,14 +2,10 @@ const bcrypt = require("bcrypt");
 const knex = require("../database/config");
 const jwt = require("jsonwebtoken");
 
-// const hash = process.env.JWT_HASH;
+const hash = process.env.JWT_HASH;
 
 async function login(req, res) {
   const { email, senha } = req.body;
-
-  if (!email || !senha) {
-    return res.status(404).json("Por favor informe e-mail e senha!");
-  }
 
   try {
     const usuario = await knex("usuarios").where({ email }).first();
@@ -18,21 +14,20 @@ async function login(req, res) {
       return res.status(404).json({ mensagem: "Usuário não encontrado" });
     }
 
-    // const correctPassword = await bcrypt.compare(senha, usuario.senha);
+    const correctPassword = await bcrypt.compare(senha, usuario.senha);
 
-    // if (!correctPassword) {
-    //   return res.status(400).json({ mensagem: "E-mail ou senha inválida" });
-    // }
+    if (!correctPassword) {
+      return res.status(400).json({ mensagem: "E-mail ou senha inválida" });
+    }
 
-    // const token = jwt.sign({ id: usuario.id }, hash, { expiresIn: "8h" });
+    const token = jwt.sign({ id: usuario.id }, hash, { expiresIn: "8h" });
 
-    const { senha: _, ...dadosUsuario } = usuario;
+    const { senha: _, telefone, cpf, ...dadosUsuario } = usuario;
 
-    // return res.status(200).json({ usuario: dadosUsuario, token });
-
-    return res.status(200).json({ usuario: dadosUsuario });
+    return res.status(200).json({ usuario: dadosUsuario, token });
   } catch (error) {
     console.log(error.message);
+
     return res.status(500).json({ mensagem: "Erro interno do servidor" });
   }
 }
--- src/validations/loginSchema.js
+++ src/validations/loginSchema.js
@@ -1,19 +1,21 @@
 const joi = require("joi");
 
-const loginSchema = joi.object({
-  email: joi.string().email().trim().strict().required().messages({
-    "any.required": "O campo nome é obrigatório",
-    "string.empty": "O campo nome é obrigatório",
-    "string.email": "Por favor informe um email válido",
-    "string.trim": "Por favor informe um email válido",
-  }),
+const loginSchema = joi
+  .object({
+    email: joi.string().email().trim().strict().required().messages({
+      "any.required": "O campo email é obrigatório",
+      "string.empty": "O campo email é obrigatório",
+      "string.email": "Por favor informe um email válido",
+      "string.trim": "Por favor informe um email válido",
+    }),
 
-  senha: joi.string().min(8).trim().required().messages({
-    "any.required": "O campo senha é obrigatório",
-    "string.empty": "O campo senha é obrigatório",
-    "string.min": "Por favor informe a senha (mínimo de 8 caracteres)",
-    "string.trim": "Por favor insira uma senha válida",
-  }),
-});
+    senha: joi.string().min(8).trim().required().messages({
+      "any.required": "O campo senha é obrigatório",
+      "string.empty": "O campo senha é obrigatório",
+      "string.min": "Por favor informe a senha (mínimo de 8 caracteres)",
+      "string.trim": "Por favor insira uma senha válida",
+    }),
+  })
+  .min(2);
 
 module.exports = loginSchema;
