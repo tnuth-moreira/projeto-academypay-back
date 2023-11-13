5b5336a43a5598a748f4a3bd9b35fed456fbef9c
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-13 19:57:04 -0300

    feat: validateRegisterClient refatorado

---
+++ src/middleware/validateRegisterClient.js
@@ -0,0 +1,13 @@
+const validateRegisterClient = (joiSchema) => async (req, res, next) => {
+  const { nome, email, cpf, telefone } = req.body;
+  try {
+    await joiSchema.validateAsync({ nome, email, cpf, telefone });
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ mensagem: error.message });
+  }
+};
+
+module.exports = validateRegisterClient;
