dad0b1284743d22b1ba9eed10fe18e54edec8b4f
Isabella Brito <isadocarmo7@gmail.com>
2023-11-18 20:35:54 -0300

    Ajuste do cors para versão que foi feito o deploy

--- src/index.js
+++ src/index.js
@@ -5,22 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-
-app.options("/registerClient", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.status(204).send();
-});
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
