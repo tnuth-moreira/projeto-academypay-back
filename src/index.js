1a8cc3bc31fa7cff634481232375ddc4baf0a700
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 18:51:44 -0300

    fix: correçao bug no cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(routes);
 
