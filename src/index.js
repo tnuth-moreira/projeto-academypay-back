5f2443fd1c2110dd246afe3fc8cb6a33ed7e31aa
Isabella Brito <isadocarmo7@gmail.com>
2023-11-18 19:03:58 -0300

    Fix corsOptions

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.options("/registerClient", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.status(204).send();
+});
+
 app.use(express.json());
 app.use(routes);
 
