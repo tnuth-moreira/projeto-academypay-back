31f2b13766f669dfb53d6bccc0af8f3eb9d35f2b
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 21:57:33 -0300

    fix: correçao bug no cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,27 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, nex) => {
+  return [
+    {
+      source: "/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+        },
+      ],
+    },
+  ];
+});
+
 app.use(express.json());
 app.use(routes);
 
