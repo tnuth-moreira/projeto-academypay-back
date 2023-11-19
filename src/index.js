b6b99c79bd5ef07273b84fad844dfc4177c09c41
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 22:32:57 -0300

    fix: correçao

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const app = express();
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/");
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+  res.header(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
-  res.setHeader(
+  res.header(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
   );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
