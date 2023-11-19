5264e478afcbb2b1c6abee0ea7facfecfe0371e1
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 22:35:36 -0300

    fix: correçao

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const routes = require("./routes");
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
   res.header(
