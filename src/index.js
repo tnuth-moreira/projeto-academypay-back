b5ccc45bea864a4b9aea11b54672819661a40c7b
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 23:33:58 -0300

    fix: cors options ultima correçao

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     preflightContinue: false,
     optionsSuccessStatus: 204,
   })
