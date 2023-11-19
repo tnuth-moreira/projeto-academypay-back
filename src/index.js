261d793a06e9460f4b529b9c9387b580a12338ae
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 22:57:22 -0300

    fix: cors options

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 require("dotenv").config();
 const routes = require("./routes");
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use(express.json());
 app.use(routes);
