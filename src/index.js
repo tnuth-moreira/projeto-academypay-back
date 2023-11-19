20d9ed9e231f414035913245fcc431eac0cece1b
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 23:08:05 -0300

    fix: cors options

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173/",
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
