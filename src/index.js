e07539815ea6f1673285cfa3243db4424e869bbe
Luiz D O Santos <lzdavi1325@gmail.com>
2023-11-18 22:37:55 -0300

    fix: cors all

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
